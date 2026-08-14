34c4ecc4524e6793e28967b07105f634c2e89037
dylanlegouellec-jpg <dylanlegouellec@gmail.com>
2026-08-14 19:06:50 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GrimoireDeMorgane from './GrimoireDeMorganev1.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GrimoireDeMorgane />
+  </React.StrictMode>,
+)
