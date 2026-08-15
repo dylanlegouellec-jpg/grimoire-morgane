caf9f606f4793a5504eaeebe54b0f8012eb1267e
dylanlegouellec-jpg <dylanlegouellec@gmail.com>
2026-08-15 08:36:07 +0200

    Delete src/supabase.js

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://mdvzdbbbueekrbzghetd.supabase.co'
-const supabaseKey = 'sb_publishable_0gw7r9y0kbMEWhitsUcNqA_cwhTKNFh' // (ou ta clé anon/publishable standard de Supabase)
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
