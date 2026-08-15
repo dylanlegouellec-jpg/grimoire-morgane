7ea69e075fbf4faeceaa957e361ea7aabbed54ff
dylanlegouellec-jpg <dylanlegouellec@gmail.com>
2026-08-15 17:12:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GrimoireDeMorgane from './GrimoireDeMorganev4.jsx'
+import GrimoireDeMorgane from './GrimoireDeMorganev5.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
